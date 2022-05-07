b2d4e0137a8cac3422f86859c0e3b0b0ae6b1010
Ryan Chang <ryanchang0501@gmail.com>
2022-05-07 13:09:57 -0400

    added assignment 0

---
+++ assignment_0/cmake-build-debug/CMakeFiles/syde223-a0.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/syde223-a0.dir/syde223-a0.cpp.o"
+  "CMakeFiles/syde223-a0.dir/syde223-a0.cpp.o.d"
+  "syde223-a0"
+  "syde223-a0.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/syde223-a0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
