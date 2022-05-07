6ae53d4683bda3e731c307d5cc173b497b16b14a
Ryan Chang <ryanchang0501@gmail.com>
2022-05-07 13:18:07 -0400

    added gitignore

--- assignment_0/cmake-build-debug/CMakeFiles/syde223-a0.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for syde223-a0.
-# This may be replaced when dependencies are built.
--- assignment_0/cmake-build-debug/cmake_install.cmake
+++ assignment_0/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /Users/rchang/CLionProjects/r28chang
+# Install script for directory: /Users/rchang/CLionProjects/syde-223/assignment_0
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/Users/rchang/CLionProjects/r28chang/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/Users/rchang/CLionProjects/syde-223/assignment_0/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
